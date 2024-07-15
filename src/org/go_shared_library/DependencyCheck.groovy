4b64bb2daad4e2555d338a85c16733e15e0259f6
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-16 01:29:43 +0530

    Update DependencyCheck.groovy

--- src/org/go_shared_library/DependencyCheck.groovy
+++ src/org/go_shared_library/DependencyCheck.groovy
@@ -4,7 +4,7 @@ class DependencyCheck {
     static void execute(script) {
                 // Run Dependency-Check using the defined tool installation
             script.sh '''
-                ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format ALL
+                ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format HTML --out dependency-check-report.html
             '''
         }
     }
