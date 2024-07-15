1a3f9b014c13f7e6b47bbf43eb541e6db014dbd1
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-16 00:45:04 +0530

    Update golangci.groovy

--- vars/golangci.groovy
+++ vars/golangci.groovy
@@ -1,5 +1,10 @@
+// vars/golangci.groovy
 import org.go_shared_library.CodeCompilation
 
 def callCodeCompilation() {
     new CodeCompilation().call()
+}
+
+def call() {
+    callCodeCompilation()
 }
