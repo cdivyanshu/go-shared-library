a2ad440acef3574243837ad194ca1d641de47b65
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-16 01:11:54 +0530

    Update golangci.groovy

--- vars/golangci.groovy
+++ vars/golangci.groovy
@@ -1,10 +1,48 @@
-// vars/golangci.groovy
-import org.go_shared_library.CodeCompilation
+package org.go_shared_library
 
-def callCodeCompilation() {
-    new CodeCompilation(this).call()
+import org.go_shared_library.Checkout
+import org.go_shared_library.CodeCompilation
+import org.go_shared_library.UnitTestStage
+import org.go_shared_library.DependencyCheck
+
+def call(Map params) {
+    String tasksToRun = params.get('tasks', 'all') // Default to 'all' tasks if not specified
+
+    switch (tasksToRun) {
+        case 'checkout':
+            checkoutGitRepository(params.gitUrl, params.gitBranch)
+            break
+        case 'CodeCompilation':
+            executeCodeCompilation()
+            break
+        case 'unitTests':
+            runUnitTestStage()
+            break
+        case 'depenCheck':
+            dependencyCheck()
+            break
+        case 'all':
+        default:
+            // Run all tasks
+            checkoutGitRepository(params.gitUrl, params.gitBranch)
+            executeGoCommands()
+            runUnitTests()
+            dependencyCheck()
+            break
+    }
 }
 
-def call() {
-    callCodeCompilation()
+def checkoutGitRepository(String gitUrl, String gitBranch) {
+    Checkout.execute(this, gitUrl, gitBranch)
+}
+
+def executeCodeCompilation() {
+    CodeCompilation.execute(this)
+}
+
+def runUnitTestStage() {
+    UnitTestStage.execute(this)
+}
+def dependencyCheck() {
+    DependencyCheck.execute(this)
 }
