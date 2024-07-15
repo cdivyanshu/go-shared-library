457776fab724395830018e401745c9a9c404b09d
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-16 00:45:53 +0530

    Update CodeCompilation.groovy

--- src/org/go_shared_library/CodeCompilation.groovy
+++ src/org/go_shared_library/CodeCompilation.groovy
@@ -1,3 +1,15 @@
+// src/org/go_shared_library/CodeCompilation.groovy
+package org.go_shared_library
+
+class CodeCompilation {
+    def call() {
+        println 'Code compilation logic goes here'
+        // Add your code compilation logic here
+    }
+}
+/*
+
+
 def call() {
     sh '''
         go mod tidy
@@ -5,3 +17,4 @@ def call() {
         go build -o employee-api .
     '''
 }
+*/
