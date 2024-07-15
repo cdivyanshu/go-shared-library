65fc8e17e11aca15dc451b70f310403996560534
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-16 01:03:56 +0530

    Update CodeCompilation.groovy

--- src/org/go_shared_library/CodeCompilation.groovy
+++ src/org/go_shared_library/CodeCompilation.groovy
@@ -1,25 +1,12 @@
-// src/org/go_shared_library/CodeCompilation.groovy
 package org.go_shared_library
 
-import hudson.model.*
-import jenkins.model.*
-import org.jenkinsci.plugins.workflow.cps.CpsScript
-
-class CodeCompilation implements Serializable {
-    def script
-
-    CodeCompilation(CpsScript script) {
-        this.script = script
-    }
-
-    def call() {
-        script.println 'Code compilation logic goes here'
-        script.dir('employee-api') {
-            script.sh '''
-                go mod tidy
-                go mod download
-                go build -o employee-api .
-            '''
-        }
+class CodeCompilation {
+    static void execute(script) {
+        // Using the Go Plugin to execute Code Compilation
+        script.sh '''
+            go mod tidy
+            go mod download
+            go build -o employee-api .
+        '''
     }
 }
