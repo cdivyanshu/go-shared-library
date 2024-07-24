8842df0019f8458811af706bccf5ac42f3f6b7cd
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-24 16:50:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 # Declare VPC resource
 resource "aws_vpc" "ot_microservices_dev" {
   cidr_block = "10.0.0.0/16"
@@ -7,10 +11,31 @@ resource "aws_vpc" "ot_microservices_dev" {
   }
 }
 
+# Declare Subnets
+resource "aws_subnet" "application_subnet" {
+  vpc_id            = aws_vpc.ot_microservices_dev.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "application-subnet"
+  }
+}
+
+resource "aws_subnet" "secondary_subnet" {
+  vpc_id            = aws_vpc.ot_microservices_dev.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "secondary-subnet"
+  }
+}
+
 # Declare ALB Security Group
 resource "aws_security_group" "alb_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
-  name = "alb-security-group"
+  name   = "alb-security-group"
 
   tags = {
     Name = "alb-security-group"
@@ -20,17 +45,17 @@ resource "aws_security_group" "alb_security_group" {
 # Declare Bastion Security Group
 resource "aws_security_group" "bastion_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
-  name = "bastion-security-group"
+  name   = "bastion-security-group"
 
   tags = {
     Name = "bastion-security-group"
   }
 }
 
-# EMPLOYEE
+# EMPLOYEE Security Group
 resource "aws_security_group" "employee_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
-  name = "employee-security-group"
+  name   = "employee-security-group"
 
   tags = {
     Name = "employee-security-group"
@@ -55,7 +80,6 @@ resource "aws_security_group" "employee_security_group" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
   }
 }
 
@@ -85,6 +109,18 @@ resource "aws_lb_target_group_attachment" "employee_target_group_attachment" {
   target_group_arn = aws_lb_target_group.employee_target_group.arn
   target_id        = aws_instance.employee_instance.id
   port             = 8080
+}
+
+# ALB Listener
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.employee_target_group.arn
+  }
 }
 
 # Listener Rule
@@ -147,7 +183,7 @@ resource "aws_autoscaling_group" "employee_autoscaling" {
     id      = aws_launch_template.employee_launch_template.id
     version = "$Default"
   }
-  vpc_zone_identifier = [aws_subnet.application_subnet.id]
+  vpc_zone_identifier = [aws_subnet.application_subnet.id, aws_subnet.secondary_subnet.id]
   target_group_arns   = [aws_lb_target_group.employee_target_group.arn]
 }
 
