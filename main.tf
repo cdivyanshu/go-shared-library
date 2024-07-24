89fc15433419fd43eb6381844cf25538a64235ae
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-24 16:43:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,6 @@ resource "aws_subnet" "secondary_subnet" {
   availability_zone = "ap-south-1b"
 }
 
-resource "aws_lb" "front_end" {
-  name               = "front-end"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_security_group.id]
-  subnets            = [aws_subnet.application_subnet.id, aws_subnet.secondary_subnet.id]
-}
-
 resource "aws_security_group" "alb_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
 }
@@ -38,22 +30,20 @@ resource "aws_security_group" "employee_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
 }
 
-resource "aws_instance" "employee_instance" {
-  ami           = "ami-0ad21ae1d0696ad58"
-  instance_type = "t2.micro"
-  key_name      = "devinfra"
-  vpc_security_group_ids = [aws_security_group.employee_security_group.id]
-  subnet_id     = aws_subnet.application_subnet.id
+resource "aws_lb" "front_end" {
+  name               = "front-end"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [aws_subnet.application_subnet.id, aws_subnet.secondary_subnet.id]
 }
 
-resource "aws_autoscaling_group" "employee_autoscaling" {
-  launch_template = {
-    id = aws_launch_template.employee_launch_template.id
-  }
-  min_size           = 1
-  max_size           = 2
-  desired_capacity   = 1
-  vpc_zone_identifier = [aws_subnet.application_subnet.id, aws_subnet.secondary_subnet.id]
+resource "aws_instance" "employee_instance" {
+  ami                    = "ami-0ad21ae1d0696ad58"
+  instance_type          = "t2.micro"
+  key_name               = "devinfra"
+  vpc_security_group_ids = [aws_security_group.employee_security_group.id]
+  subnet_id              = aws_subnet.application_subnet.id
 }
 
 resource "aws_launch_template" "employee_launch_template" {
@@ -61,6 +51,17 @@ resource "aws_launch_template" "employee_launch_template" {
   image_id      = "ami-0ad21ae1d0696ad58"
   instance_type = "t2.micro"
   key_name      = "devinfra"
+}
+
+resource "aws_autoscaling_group" "employee_autoscaling" {
+  launch_template {
+    id      = aws_launch_template.employee_launch_template.id
+    version = "$Latest"
+  }
+  min_size            = 1
+  max_size            = 2
+  desired_capacity    = 1
+  vpc_zone_identifier = [aws_subnet.application_subnet.id, aws_subnet.secondary_subnet.id]
 }
 
 resource "aws_lb_target_group" "employee_target_group" {
